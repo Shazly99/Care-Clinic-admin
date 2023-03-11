8102f677be07ff13f481716140e7e47028e6ad67
Ahmed Elshazly <57854391+Shazly99@users.noreply.github.com>
2023-03-11 14:06:58 +0200

    Update Component.js

--- src/constants/Component.js
+++ src/constants/Component.js
@@ -1,1 +1,79 @@
 
+import Vendor from '../Layout/Vendor'; 
+import Users from './../Pages/Vendor/Users/Users'; 
+import Navber from './../Components/Navber/Navber';
+import Sildebar from './../Components/Sidebar/Sildebar';
+import ButtonBase from './../Components/Button/ButtonBase';
+import UsersTable from './../Pages/Vendor/Users/UsersTable';  
+import SubNav from './../Components/Navber/SubNav';
+import BaseHeader from './../Components/Header/BaseHeader';  
+import Auth from './../Layout/Auth';
+import Login from './../Pages/auth/Login/Login';
+import AddNewUser from '../Pages/Vendor/Users/AddUser/AddNewUser';
+import Edit from './../Pages/Vendor/Users/Edit/Edit'; 
+import Brand from './../Pages/Vendor/Brand/Brand';
+// rashed
+import AddSec2 from '../Pages/Vendor/Sec2/AddSec2/AddSec2';
+import EditSec2 from '../Pages/Vendor/Sec2/EditSec2/EditSec2';
+import Sec2 from '../Pages/Vendor/Sec2/Sec2';
+import Sec2Table from '../Pages/Vendor/Sec2/Sec2Table';
+import Loader from '../Components/Shared/Loader/Loader';
+
+import AddBrand from './../Pages/Vendor/Brand/AddBrand';
+import EditBrand from './../Pages/Vendor/Brand/EditBrand';
+import Contactus from './../Pages/Vendor/Contactus/Contactus';
+import AddContactus from './../Pages/Vendor/Contactus/AddContactus';
+import EditContactus from './../Pages/Vendor/Contactus/EditContactus';
+import Sec1 from './../Pages/Vendor/Sec1/Sec1';
+import AddSec1 from './../Pages/Vendor/Sec1/AddSec1';
+import EditSec1 from './../Pages/Vendor/Sec1/EditSec1';
+import Sec3 from './../Pages/Vendor/Sec3/Sec3';
+import AddSec3 from './../Pages/Vendor/Sec3/AddSec3';
+import EditSec3 from './../Pages/Vendor/Sec3/EditSec3';
+import Sec4 from './../Pages/Vendor/Sec4/Sec4';
+import AddSec4 from './../Pages/Vendor/Sec4/AddSec4';
+import EditSec4 from './../Pages/Vendor/Sec4/EditSec4';
+
+export default {
+    // Layout 
+    Vendor,
+    Auth, 
+// rashed
+AddSec2,
+EditSec2,
+Sec2,
+Sec2Table,
+Loader,
+    // Vendor Components User 1 
+    AddNewUser,
+    Users,
+    UsersTable, 
+    Edit, 
+
+    // Auth
+    Login,
+
+    //Brand
+    Brand,
+    AddBrand,
+    EditBrand,
+    Contactus,
+    AddContactus,
+    EditContactus,
+    Sec1,
+    AddSec1,
+    EditSec1,
+    Sec3,
+    AddSec3,
+    EditSec3,
+    Sec4,
+    AddSec4,
+    EditSec4,
+    // Components
+    Navber,
+    SubNav,
+    Sildebar, 
+    ButtonBase,
+    BaseHeader
+}
+
